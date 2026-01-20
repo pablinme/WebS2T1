451b4d60077670ab5cce39c775792d48a12323ab
Pablo Miranda <pablo.miranda@hsbcad.com>
2026-01-20 07:41:51 -0500

    rebuild

--- obj/Debug/net8.0/S2T1.AssemblyInfo.cs
+++ obj/Debug/net8.0/S2T1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S2T1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493302eabbe7cde11258b79ac12d7330a125510b")]
 [assembly: System.Reflection.AssemblyProductAttribute("S2T1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S2T1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
